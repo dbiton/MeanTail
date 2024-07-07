023a07bbdd0595976170bb01239a447038bffb71
dbiton <dbiton@gmail.com>
2024-07-07 23:19:13 +0300

    Add simple test for uniform distribution

--- test_dist_counters.py
+++ test_dist_counters.py
@@ -1,4 +1,5 @@
 import unittest
+from random import randint
 
 from dist_counters import DistCounters
 
@@ -28,6 +29,21 @@ class TestDistCountersSingular(unittest.TestCase):
         self.dist_counters.update(1, 10)
         self.dist_counters.update(1, -4)
         self.assertEqual(self.dist_counters.query(1), 6, "Counter value should be updated to 6 after adding -4")
+
+class TestDistCountersUniform(unittest.TestCase):
+
+    def setUp(self):
+        self.counters_count = 10
+        self.keys_count = 100
+        distribution = lambda index : 1 / self.keys_count
+        self.dist_counters = DistCounters(self.counters_count, distribution)
+
+    def test_overfilled_tracked_keys(self):
+        for i in [randint(0, self.keys_count-1) for _ in range(self.keys_count)]:
+          self.dist_counters.update(i, 1)
+        for i in self.dist_counters.keys:
+          self.assertEqual(self.dist_counters.query(i), 1, "Counter value should be updated to 1")
+
 
 
 if __name__ == '__main__':
