e0c227ee42bb847d6410d7ece981def1f8820ac2
dbiton <dbiton@gmail.com>
2024-07-08 15:47:33 +0300

    Implement replacing far apart keys

--- dist_counters.py
+++ dist_counters.py
@@ -1,51 +1,62 @@
 from random import randrange
 
+
 class DistCounters:
-  def __init__(self, size, distribution):
-    self.distribution = distribution
-    self.size = size
-    self.keys = []
-    self.total_counter = 0
+    def __init__(self, size, distribution):
+        self.distribution = distribution
+        self.size = size
+        self.keys = []
+        self.total_counter = 0
 
-  def estimate_counter(self, index):
-    return self.distribution(index + 1) * self.total_counter 
+    def estimate_counter(self, index):
+        return self.distribution(index + 1) * self.total_counter
 
-  def swap_probability(self, index, value):
-    value_small = self.estimate_counter(index)
-    value_large = self.estimate_counter(index - 1)
-    difference = value_large - value_small
-    if difference < 0:
-      x = 0
-    if difference == 0:
-      return 0
-    # consider swapping more than a single step up
-    probability = min(value / difference, 1)
-    return probability
+    def rebalance_estimate(self, index, value):
+        curr_index = index
+        tresh = self.swap_probability(curr_index, value)
+        while tresh >= 1:
+            self.swap(curr_index)
+            curr_index -= 1
+            tresh = self.swap_probability(curr_index, value)
+        if randrange(0, 1) < tresh and curr_index > 0:
+            self.swap(curr_index)
 
-  def swap(self, index):
-    tmp = self.keys[index]
-    self.keys[index] = self.keys[index-1]
-    self.keys[index-1] = tmp
-  
-  def update(self, key, value):
-    self.total_counter += value
-    if self.keys.count(key) > 0:
-      index = self.keys.index(key)
-      if index > 0:
-        tresh = self.swap_probability(index, value)
-        if randrange(0, 1) >= tresh:
-          self.swap(index)
-    else:
-      if len(self.keys) < self.size:
-        self.keys.append(key)
-      else:
-        tresh = self.swap_probability(self.size, value)
-        if randrange(0, 1) >= tresh:
-          self.keys[-1] = key
+    def swap_probability(self, index, value):
+        if index == 0:
+            return 0
+        value_small = self.estimate_counter(index)
+        value_large = self.estimate_counter(index - 1)
+        difference = value_large - value_small
+        if difference < 0:
+            x = 0
+        if difference == 0:
+            return 1
+        # consider swapping more than a single step up
+        probability = min(value / difference, 1)
+        return probability
 
-  def query(self, key):
-    if self.keys.count(key) > 0:
-      index = self.keys.index(key)
-      return self.estimate_counter(index)
-    else:
-      return 0
+    def swap(self, index):
+        tmp = self.keys[index]
+        if len(self.keys) < index:
+            x = 3
+        self.keys[index] = self.keys[index - 1]
+        self.keys[index - 1] = tmp
+
+    def update(self, key, value):
+        self.total_counter += value
+        if self.keys.count(key) > 0:
+            index = self.keys.index(key)
+            if index > 0:
+                self.rebalance_estimate(index, value)
+        else:
+            self.keys.append(key)
+            self.rebalance_estimate(len(self.keys)-1, value)
+            while len(self.keys) >= self.size:
+                self.keys.pop()
+
+    def query(self, key):
+        if self.keys.count(key) > 0:
+            index = self.keys.index(key)
+            return self.estimate_counter(index)
+        else:
+            return 0
