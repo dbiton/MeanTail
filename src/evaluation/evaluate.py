18d23cb51b6911da28b0d933af6a7567650131e0
dbiton <dbiton@gmail.com>
2024-07-12 00:08:35 +0300

    Add a plot for error per item

--- src/evaluation/evaluate.py
+++ src/evaluation/evaluate.py
@@ -14,6 +14,7 @@ from random import randint
 from time import time
 import numpy as np
 
+import matplotlib.pyplot as plt
 
 def evaluate(estimator, stream):
     for e in stream:
@@ -21,31 +22,52 @@ def evaluate(estimator, stream):
     actual_counts = Counter(stream)
     estimate_counts = {k: estimator.query(k) for k in actual_counts}
     diff_counts = {k: abs(estimate_counts[k]-a) for k, a in actual_counts.items()}
-    return sum(diff_counts.values()) / len(actual_counts)
+    diff_counts_values_sorted = sorted(diff_counts.values(), reverse=True)    
+    return sum(diff_counts.values()) / len(actual_counts), diff_counts_values_sorted
+
 
 
 if __name__ == "__main__":
     np.random.seed(42069)
-    stream_size = 50000
-    key_count = 5000
-    estimator_size = 1600
-    cm_depth = 4
+    stream_size = 1000000
+    key_count = 100000
+    estimator_size = 10000
+    cm_depth = 5
     dists = [
         dist.ExponentialDistribution(key_count),
         dist.NormalDistribution(key_count),
         dist.UniformDistribution(key_count),
     ]
+    
+    data = {}
+    
     for d in dists:
-        logger.info(d.__class__.__name__)
+        probability_name = d.__class__.__name__
+        logger.info(probability_name)
         probability_function = d.probability
-        ss = SpaceSaving(estimator_size / 2)
+        ss = SpaceSaving(estimator_size // 1.5)
         dc = DistCounters(estimator_size, probability_function)
         cm = CountMin(estimator_size // cm_depth, cm_depth)
         stream = d.generate(stream_size)
 
+        data[probability_name] = {}
+        
         for estimator in [ss, dc, cm]:
             t0 = time()
-            ss_error = evaluate(estimator, stream)
+            ss_error, deltas = evaluate(estimator, stream)
             t1 = time()
             estimator_name = estimator.__class__.__name__
             logger.info(f"[{estimator_name}] aae: {round(ss_error,2)} time: {round(t1 - t0,2)}")
+            data[probability_name][estimator_name] = deltas
+    
+    fig, axs = plt.subplots(nrows=len(data),ncols=len(data), figsize=(10, 10))
+    for i_prob, probability_name in enumerate(data):
+        for i_est, estimator_name in enumerate(data[probability_name]):
+            ax = axs[i_prob][i_est]
+            diff_counts_values_sorted = data[probability_name][estimator_name]
+            ax.plot(np.arange(len(diff_counts_values_sorted)), diff_counts_values_sorted)
+            ax.set_xlabel('rank')
+            ax.set_ylabel('absolute error')
+            ax.set_title(f'{probability_name} {estimator_name}')
+    plt.tight_layout()
+    plt.show()
