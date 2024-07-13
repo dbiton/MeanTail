b968ce9e0fbb581148e85367688558500bdc4091
dbiton <dbiton@gmail.com>
2024-07-13 20:07:09 +0300

    Implement frequent

---
+++ src/estimators/frequent.py
@@ -0,0 +1,19 @@
+class SpaceSaving:
+    def __init__(self, size):
+        self.decrements = 0
+        self.size = size
+        self.counters = {}
+
+    def update(self, index, value):
+        if index in self.counters:
+            self.counters[index] += value
+        else:
+            if len(self.counters) >= self.size:
+                self.decrements += 1
+                keys_to_remove = [k for k, v in self.counters.items() if v - self.decrements <= 0]
+                self.counters = {k: v for k, v in self.counters.items() if k not in keys_to_remove}
+            if len(self.counters) < self.size:
+                self.counters[index] = value
+
+    def query(self, index):
+        return self.counters.get(index, 0) - self.decrements
