42671e6a82fbc43ce0b81a68f194f0b095e124e7
dbiton <dbiton@gmail.com>
2024-11-05 11:02:42 +0200

    Use memory usage instead of estimator length

--- src/evaluation/paper.py
+++ src/evaluation/paper.py
@@ -17,6 +17,7 @@ from estimators.mean_tail import MeanTail
 from multiprocessing import Pool
 from datetime import datetime
 
+
 # Function to read trace files
 def read_trace(file_path, n=None):
     with open(file_path, "r") as file:
@@ -25,15 +26,19 @@ def read_trace(file_path, n=None):
         else:
             return [int(line.strip()) for line in file]
 
+
 def calculate_mse(estimator, actual_counts):
     errors = np.zeros(len(actual_counts))
     i = 0
     estimates = [estimator.query(k) for k in actual_counts]
-    errors = [(v_est - v)**2 for v_est, v in zip(estimates, actual_counts.values())]
+    errors = [(v_est - v) ** 2 for v_est, v in zip(estimates, actual_counts.values())]
     return np.average(errors)
 
+
 def calculate_recall(estimator, actual_counts, count_hh):
-    top_k_pairs = sorted(actual_counts.items(), key=lambda item: item[1], reverse=True)[:count_hh]
+    top_k_pairs = sorted(actual_counts.items(), key=lambda item: item[1], reverse=True)[
+        :count_hh
+    ]
     top_k = [p[0] for p in top_k_pairs]
     estimates = [estimator.query(k) for k in top_k]
     TP = len([e for e in estimates if e > 0])
@@ -41,30 +46,36 @@ def calculate_recall(estimator, actual_counts, count_hh):
     recall = TP / (TP + FN)
     return recall
 
+
+def calculate_mt_length(int_count, tail_percentage):
+    length = int_count / (2 - tail_percentage)
+    return length
+
 # Process each trace file
 def process_trace(trace_file):
-    linestyles = ['-', '--', ':', "-."] * 2 
-    markers = ['o', 's', 'D', '^', '*', '>'] * 2
-    trace_len = 1000
+    linestyles = ["-", "--", ":", "-."] * 2
+    markers = ["o", "s", "D", "^", "*", ">"] * 2
+    trace_len = 100000
     recall_heavy_hitters = 1000
 
     # Get the start time
     start_time = datetime.now()
     print(f"Start processing {trace_file} at {start_time}")
-    
+
     trace = read_trace(trace_file, trace_len)
     actual_counts = Counter(trace)
 
-    estimator_lengths = []
+    memory_usages = []
     mse_values = {}
     recall_values = {}
 
     log2_count_keys = math.ceil(math.log2(len(actual_counts)))
 
     # Run experiments with different estimators
-    for estimator_exp in np.linspace(log2_count_keys-3,log2_count_keys,20):
-        estimator_length = 2 ** estimator_exp
-        estimator_lengths.append(estimator_length)
+    for estimator_exp in np.linspace(log2_count_keys-1, log2_count_keys+3, 20):
+        memory_usage_bytes = 2**estimator_exp
+        memory_usages.append(memory_usage_bytes)
+        estimator_length = memory_usage_bytes // 8
         estimators = {
             # "FR": Frequent(estimator_length),
             # "SS": SpaceSaving(estimator_length),
@@ -74,6 +85,7 @@ def process_trace(trace_file):
             "MT8": MeanTail(estimator_length, 0.125),
             "MT4": MeanTail(estimator_length, 0.25),
         }
+
         for estimator_name, estimator in estimators.items():
             for k in trace:
                 estimator.update(k, 1)
@@ -85,44 +97,54 @@ def process_trace(trace_file):
             mse_values[estimator_name].append(mse)
             recall_values[estimator_name].append(recall)
 
-
     # Plotting the results
     plt.figure()
     i = 0
     for estimator_name, mse_list in mse_values.items():
-        plt.plot(estimator_lengths, mse_list, label=estimator_name, marker=markers[i], linestyle=linestyles[i])
+        plt.plot(
+            memory_usages,
+            mse_list,
+            label=estimator_name,
+            marker=markers[i],
+            linestyle=linestyles[i],
+        )
         i += 1
 
     plt.xscale("log", base=2)
     plt.yscale("log", base=10)
-    plt.xlabel("Estimator Length")
+    plt.xlabel("Memory Usage (Bytes)")
     plt.ylabel("Mean Squared Error")
     plt.tight_layout()
-    
+
     # Save the graph with the trace file name
     output_file = f"mse_{os.path.splitext(os.path.basename(trace_file))[0]}.png"
     plt.savefig(output_file)
 
-    legend = plt.legend(ncol=len(estimators))    
+    legend = plt.legend(ncol=len(estimators))
     # save legend as figure
-    fig  = legend.figure
+    fig = legend.figure
     fig.canvas.draw()
     bbox = legend.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
-    fig.savefig('legend', dpi="figure", bbox_inches=bbox)
-    
+    fig.savefig("legend", dpi="figure", bbox_inches=bbox)
 
     # Plotting the results
     plt.figure()
     i = 0
     for estimator_name, recall_list in recall_values.items():
-        plt.plot(estimator_lengths, recall_list, label=estimator_name, marker=markers[i], linestyle=linestyles[i])
+        plt.plot(
+            memory_usages,
+            recall_list,
+            label=estimator_name,
+            marker=markers[i],
+            linestyle=linestyles[i],
+        )
         i += 1
 
-    plt.xlabel("Estimator Length")
+    plt.xlabel("Memory Usage (Bytes)")
     plt.ylabel("Recall")
     plt.legend()
     plt.tight_layout()
-    
+
     # Save the graph with the trace file name
     output_file = f"recall_{os.path.splitext(os.path.basename(trace_file))[0]}.png"
     plt.savefig(output_file)
@@ -132,16 +154,22 @@ def process_trace(trace_file):
     print(f"Finished processing {trace_file} at {end_time}, saved as {output_file}")
     print(f"Duration: {end_time - start_time}")
 
+
 # Main function to execute in parallel
 def main():
     trace_dir = "./src/traces"
-    
+
     # Get all .trace files in the directory
-    trace_files = [os.path.join(trace_dir, f) for f in os.listdir(trace_dir) if f.endswith(".trace")]
+    trace_files = [
+        os.path.join(trace_dir, f)
+        for f in os.listdir(trace_dir)
+        if f.endswith(".trace")
+    ]
 
     # Run the trace processing in parallel
     with Pool() as pool:
         pool.map(process_trace, trace_files)
+
 
 if __name__ == "__main__":
     main()
